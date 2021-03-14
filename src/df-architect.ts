5e5a7792ff74329186c18a403f76ae9c9569b735
flamewave000 <3588046+flamewave000@users.noreply.github.com>
2021-03-13 21:26:26 -0500

    Ctrl invert functioning mostly
    resolves #1
    
    Currently working, though the chaining requires a click+drag instead of an expected click to place subsequent walls.

--- src/df-architect.ts
+++ src/df-architect.ts
@@ -3,17 +3,19 @@ import ARCHITECT from "./core/architect.js";
 import HOTKEYS from "./core/hotkeys.js";
 import { LayerShortcuts } from "./general/LayerShortcuts.js";
 import { AltGridSnap } from './general/AltGridSnap.js';
+import { WallCtrlInvert } from './walls/WallCtrlInvert.js';
 
 
 Hooks.once('init', function () {
 	ARCHITECT.DrawArchitectGraphicToConsole();
 	HOTKEYS.init();
 	AltGridSnap.init();
+	WallCtrlInvert.init();
 });
 
-Hooks.once('setup', function () {
-});
+Hooks.once('setup', function () { });
 
 Hooks.once('ready', function () {
 	LayerShortcuts.ready();
+	WallCtrlInvert.ready();
 });
---
+++ src/walls/WallCtrlInvert.ts
@@ -0,0 +1,113 @@
+import SETTINGS from "../core/settings.js";
+
+
+interface KeyboardCallbacks {
+	[key: string]: any
+	clickLeft(event: PIXI.InteractionEvent): void
+	clickLeft2(event: PIXI.InteractionEvent): void
+	clickRight(event: PIXI.InteractionEvent): void
+	dragLeftStart(event: PIXI.InteractionEvent): void
+	dragLeftMove(event: PIXI.InteractionEvent): void
+	dragLeftDrop(event: PIXI.InteractionEvent): void
+	dragLeftCancel(event: PIXI.InteractionEvent): void
+	dragRightMove(event: PIXI.InteractionEvent): void
+	dragRightDrop(event: PIXI.InteractionEvent): void
+}
+
+class _WallCtrlInvert {
+	static PREF_ENABLED = 'WallCtrlInvert-Enabled';
+
+	private _originalCallbacks: KeyboardCallbacks = null;
+	private _overrideCallbacks: KeyboardCallbacks = {} as KeyboardCallbacks;
+
+	get enabled(): boolean { return SETTINGS.get(_WallCtrlInvert.PREF_ENABLED) }
+	set enabled(value: boolean) { SETTINGS.set(_WallCtrlInvert.PREF_ENABLED, value) }
+
+	init() {
+		SETTINGS.register(_WallCtrlInvert.PREF_ENABLED, {
+			scope: 'world',
+			config: false,
+			type: Boolean,
+			default: false,
+			onChange: () => this._patchWallsLayer()
+		});
+
+		Hooks.on('getSceneControlButtons', (controls: SceneControl[]) => {
+			const isGM = game.user.isGM;
+			const wallsControls = controls.find(x => x.name === 'walls');
+			wallsControls.tools.splice(wallsControls.tools.findIndex(x => x.name === 'snap'), 0, {
+				icon: 'fas fa-link',
+				name: 'ctrlInvert',
+				title: 'DF_ARCHITECT.WallCtrlInvert_Label',
+				visible: isGM,
+				toggle: true,
+				active: this.enabled,
+				onClick: (toggled: boolean) => { this.enabled = toggled }
+			});
+		});
+	}
+
+	ready() {
+		this._patchWallsLayer();
+	}
+
+	private wrap(original: any) {
+		const wrapper = function (this: any, event: KeyboardEvent | PIXI.InteractionEvent) {
+			if (event)
+				event = _WallCtrlInvert._invertCtrl(event);
+			return this._original(event);
+		}
+		wrapper._original = original;
+		return wrapper.bind(wrapper);
+	}
+
+	private _patchWallsLayer() {
+		const mim = (canvas as Canvas).mouseInteractionManager;
+		if (!this._originalCallbacks) {
+			this._originalCallbacks = mim.callbacks as KeyboardCallbacks;
+			for (let key of Object.keys(this._originalCallbacks)) {
+				if (this._originalCallbacks[key] == null)
+					this._overrideCallbacks[key] = null
+				else
+					this._overrideCallbacks[key] = this.wrap(this._originalCallbacks[key]);
+			}
+		}
+		mim.callbacks = this.enabled ? this._overrideCallbacks : this._originalCallbacks;
+	}
+
+	private static _invertCtrl(event: KeyboardEvent | PIXI.InteractionEvent): KeyboardEvent | PIXI.InteractionEvent {
+		if (ui.controls.activeControl !== 'walls' || ui.controls.activeTool === 'select')
+			return event;
+		if ("ctrlKey" in event)
+			return event instanceof _KeyboardEvent ? event : new _KeyboardEvent(event) as any as KeyboardEvent;
+		else {
+			const data: { originalEvent: KeyboardEvent } = (event as any).data;
+			if (!(data.originalEvent instanceof _KeyboardEvent))
+				data.originalEvent = new _KeyboardEvent(data.originalEvent) as any as KeyboardEvent;
+			return event;
+		}
+	}
+}
+export const WallCtrlInvert = new _WallCtrlInvert();
+
+class _KeyboardEvent {
+	private _event: KeyboardEvent;
+	constructor(event: KeyboardEvent) {
+		this._event = event;
+	}
+	get altKey(): boolean { return this._event.altKey; }
+	get code(): string { return this._event.code; }
+	get ctrlKey(): boolean { return !this._event.ctrlKey; }
+	get isComposing(): boolean { return this._event.isComposing; }
+	get key(): string { return this._event.key; }
+	get location(): number { return this._event.location; }
+	get metaKey(): boolean { return this._event.metaKey; }
+	get repeat(): boolean { return this._event.repeat; }
+	get shiftKey(): boolean { return this._event.shiftKey; }
+	getModifierState(keyArg: string): boolean { return this._event.getModifierState(keyArg); }
+	preventDefault() { this._event.preventDefault(); }
+	get DOM_KEY_LOCATION_LEFT(): number { return this._event.DOM_KEY_LOCATION_LEFT; }
+	get DOM_KEY_LOCATION_NUMPAD(): number { return this._event.DOM_KEY_LOCATION_NUMPAD; }
+	get DOM_KEY_LOCATION_RIGHT(): number { return this._event.DOM_KEY_LOCATION_RIGHT; }
+	get DOM_KEY_LOCATION_STANDARD(): number { return this._event.DOM_KEY_LOCATION_STANDARD; }
+}
