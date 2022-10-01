b57d16be6c75ad1dd7197c6981f6c030876e06ab
flamewave000 <3588046+flamewave000@users.noreply.github.com>
2022-10-01 17:07:24 -0400

    V10: Fix Wall Split/Join function
    Fix #152
    Fix #153

--- src/walls/WallJoinSplit.ts
+++ src/walls/WallJoinSplit.ts
@@ -32,15 +32,15 @@ export default class WallJoinSplit {
 		const walls = layer.controlled;
 		const newWalls: WallData[] = [];
 		for (let wall of walls) {
-			const [x1, y1, x2, y2] = wall.data.c;
+			const [x1, y1, x2, y2] = wall.document.c;
 			var midX = (x1 + x2) / 2;
 			var midY = (y1 + y2) / 2;
 			//@ts-expect-error
 			[midX, midY] = layer._getWallEndpointCoordinates(new PIXI.Point(midX, midY), {snap:false});
-			const wall1 = duplicate(wall.data) as WallData;
+			const wall1 = duplicate(wall.document._source) as WallData;
 			wall1.c = [x1, y1, midX, midY];
 			delete wall1._id;
-			const wall2 = duplicate(wall.data) as WallData;
+			const wall2 = duplicate(wall.document._source) as WallData;
 			wall2.c = [midX, midY, x2, y2];
 			delete wall2._id;
 			newWalls.push(wall1, wall2)
@@ -59,7 +59,7 @@ export default class WallJoinSplit {
 		const layer = canvas.walls;
 		const walls = layer.controlled;
 		for (let wall of walls) {
-			const [x1, y1, x2, y2] = wall.data.c;
+			const [x1, y1, x2, y2] = wall.document.c;
 			//@ts-expect-error
 			points.getOrDefault(JSON.stringify(layer._getWallEndpointCoordinates(new PIXI.Point(x1, y1), {snap:false})), []).push(wall);
 			//@ts-expect-error
@@ -70,7 +70,7 @@ export default class WallJoinSplit {
 			return;
 		}
 		const endpoints = [...points.entries()].filter(x => x[1].length == 1);
-		const wallData = duplicate(endpoints[0][1][0].data) as WallData;
+		const wallData = duplicate(endpoints[0][1][0].document._source) as WallData;
 		delete wallData._id;
 		wallData.c = endpoints.reduce((r, x) => r.concat(JSON.parse(x[0])), [] as number[]) as [number, number, number, number];
 		await game.scenes.viewed.deleteEmbeddedDocuments('Wall', walls.map(x => x.id));
